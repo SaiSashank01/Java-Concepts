4e703c14e204597464f37b498f531b7f01ac62d0
SaiSashank01 <saisashankpedapalli90@gmail.com>
2026-09-10 10:57:39 +0530

    feat(AddingTwoNumber):

---
+++ JavaFundementals/AddingTwoNumber.java
@@ -0,0 +1,8 @@
+class AddingTwoNumber{
+    public static void main(String[] args){
+        int firstNumber = 45 ;
+        int secondNumber = 67;
+        int finalValueSum = firstNumber + secondNumber;
+        System.out.println("This is Sum of two numbers : " + finalValueSum);
+    }
+}
