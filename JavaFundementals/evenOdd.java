c0cb66c2c276adf56828f5ba5e0da81b54d58514
SaiSashank01 <saisashankpedapalli90@gmail.com>
2026-09-12 11:12:56 +0530

    feat(evenOdd.java):

---
+++ JavaFundementals/evenOdd.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class evenOdd {
+    public static void main(String[] args){
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter the Value : ");
+        int Number = obj.nextInt();
+        if (Number % 2 == 0){
+            System.out.println("This is Even. "+Number);
+        }else{
+            System.out.println("This is Odd. "+Number);
+        }
+
+    }
+}
