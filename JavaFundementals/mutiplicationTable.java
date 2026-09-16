7e6f398dc2e7b06b3055bd59915166280abd9c18
Sai Sashank <saisashankpedapalli90@gmail.com>
2026-09-16 21:18:10 +0530

    Update mutiplicationTable.java

--- JavaFundementals/mutiplicationTable.java
+++ JavaFundementals/mutiplicationTable.java
@@ -1,14 +1,10 @@
 // Java program to print table of a number 
 import java.util.*;
-
 class mutiplicationTable {
-
-    public static void printTable(int n)  {
-              
+    public static void printTable(int n)  {         
         for (int i = 1; i <= 10; ++i) 
             System.out.println(n + " * " + i +" = " + n * i);
     }
-  
     public static void main(String arg[]){   
         Scanner obj = new Scanner(System.in);
         System.out.println("Enter the Number: ");
